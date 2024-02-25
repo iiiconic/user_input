85ce6cf68b7c685dd0959a47e97c5db0de6516a3
iiicon <iamiiiconic@gmail.com>
2024-02-25 20:44:19 +0000

    first commit

---
+++ input_user.py
@@ -0,0 +1,5 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+location = input('Enter your location: ')
+
+print(f'Hi. My name is {name}, I am {age} years old. I live in {location}')
